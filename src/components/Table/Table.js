314e557392c7f85656a967362627f044e8a05030
yasirghouri <yasir.ag7@gmail.com>
2021-02-13 19:21:42 +0500

    added numerals to Table component

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,20 +1,20 @@
 import React from "react";
-
 import "./Table.css";
+import numeral from "numeral";
 
-const Table = ({ countries }) => {
+function Table({ countries }) {
   return (
     <div className="table">
-      {countries.map(({ country, cases, idx }) => {
-        return (
-          <tr>
-            <td>{country}</td>
-            <td>{cases}</td>
-          </tr>
-        );
-      })}
+      {countries.map(({ country, cases }) => (
+        <tr>
+          <td>{country}</td>
+          <td>
+            <strong>{numeral(cases).format("000,000")}</strong>
+          </td>
+        </tr>
+      ))}
     </div>
   );
-};
+}
 
 export default Table;
